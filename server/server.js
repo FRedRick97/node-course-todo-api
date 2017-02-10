6d566a04f6eca08337948d57f66bf4589619ef7f
Aakash <chopra.aakash7@gmail.com>
2017-02-10 17:00:15 +0530

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser'); // take your json & convert it into an object
+var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -7,27 +7,27 @@ var {User} = require('./models/user.js');
 
 var app = express();
 
-app.use(bodyParser.json()); // takes your JSON and convert it into object, attaching it onto request.
+app.use(bodyParser.json()); 
 
 app.post('/todos', (req, res) => {
 	// console.log(req.body);
 	var todo = new Todo({
-		text: req.body.text // bodyParser
+		text: req.body.text 
 	});
 
 	todo.save().then((doc) => {
 		res.send(doc);
 	}, (e) => {
-		res.status(400).send(e); // http://httpstatues.com
+		res.status(400).send(e);
 	});
 });
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		// res.send(todos);
-		res.send({todos}); // flexible to create an object and send, this helps to add more things inside object incase we need to
+		res.send({todos});
 	}, (e) => {
-		res.send(400).send(e);
+		res.status(400).send(e);
 	});
 });
 
