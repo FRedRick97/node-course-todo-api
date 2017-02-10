b335740c71ee65163b65a77b6d886cd7bc212ced
Aakash <chopra.aakash7@gmail.com>
2017-02-10 16:22:49 +0530

    Testing server.js & Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,17 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		// res.send(todos);
+		res.send({todos}); // flexible to create an object and send, this helps to add more things inside object incase we need to
+	}, (e) => {
+		res.send(400).send(e);
+	});
+});
+
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+module.exports = {app};
