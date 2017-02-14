75ac1eaad11a92bcaccf1478ceaa4cef4e74db14
Aakash <chopra.aakash7@gmail.com>
2017-02-14 13:19:37 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,39 +1,53 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
-/*jwt.sign -> takes the object and it signs it i.e it creates hash and then returns token value*/
-/*jwt.verify -> takes token and secret and make sure data wasn't manipulated*/
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc'); /*.sign(object, secret)*/
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash); 
+// 	});
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$ykaJQpCfcJnVyV1ZgfRTGOLdAaYS1DyDA7t3yleMkr3g2tTKj4qhG';
 
-// var message = 'Hello World...!!!';
-// var hash = SHA256(message).toString(); /*converting object to string*/
-
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
-
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
 // var data = {
-// 	id: 4
+// 	id: 10
 // };
-// var token = {
-// 	data,
-// 	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// };
+// /*jwt.sign -> takes the object and it signs it i.e it creates hash and then returns token value*/
+// /*jwt.verify -> takes token and secret and make sure data wasn't manipulated*/
 
-// // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+// var token = jwt.sign(data, '123abc'); /*.sign(object, secret)*/
+// console.log(token);
 
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
-// if(resultHash === token.hash) {
-// 	console.log('Data was not changed');
-// } else {
-// 	console.log('Data was changed. Do not trust!');
-// }
+// // var message = 'Hello World...!!!';
+// // var hash = SHA256(message).toString(); /*converting object to string*/
+
+// // console.log(`Message: ${message}`);
+// // console.log(`Hash: ${hash}`);
+
+// // var data = {
+// // 	id: 4
+// // };
+// // var token = {
+// // 	data,
+// // 	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// // };
+
+// // // token.data.id = 5;
+// // // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// // var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// // if(resultHash === token.hash) {
+// // 	console.log('Data was not changed');
+// // } else {
+// // 	console.log('Data was changed. Do not trust!');
+// // }
